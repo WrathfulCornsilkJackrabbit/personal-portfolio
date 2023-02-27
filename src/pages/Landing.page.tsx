533567a6150c8bcb9843c0926b83bbb9cf9ecdaa
Wrathful Cornsilk Jackrabbit <X7dqbY6a@proton.me>
2023-02-27 17:18:42 +0000

    #1 - Remove old code

---
+++ src/pages/Landing.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <>
+            <p>Landing Page</p>
+            <p>Hero Component</p>
+            <p>Portfolio</p>
+            <p>Contact</p>
+
+        </>
+    )
+}
+
+export default LandingPage;
